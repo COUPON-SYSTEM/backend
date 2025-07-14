926725a019877e5cd53f2a3e2eb3d7dbfb9552b5
Shinjongyun <tigerjong123@gmail.com>
2025-07-14 17:29:50 +0900

    Feat: GiftIssueWorker

---
+++ src/main/java/com/company/demo/giftcoupon/mapper/dto/request/GiftRequest.java
@@ -0,0 +1,12 @@
+package com.company.demo.giftcoupon.mapper.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class GiftRequest {
+    String userId;
+}
