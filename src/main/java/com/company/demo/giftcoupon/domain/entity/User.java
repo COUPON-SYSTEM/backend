75dc1291351e4c5f4a39af33a1d20316a8b6714e
Shinjongyun <tigerjong123@gmail.com>
2025-07-14 17:06:34 +0900

    Feat: User

---
+++ src/main/java/com/company/demo/giftcoupon/domain/entity/User.java
@@ -0,0 +1,22 @@
+package com.company.demo.giftcoupon.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", nullable = false)
+    private Long id;
+
+    @Column(name = "user_name", length = 30, nullable = false)
+    private String userName;
+}
