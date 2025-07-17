96b8dcda000d807c3c1bba9b9de1f73be99cb340
Shinjongyun <tigerjong123@gmail.com>
2025-07-17 17:17:52 +0900

    Feat: TestConsumer 추가

---
+++ src/main/java/com/company/demo/giftcoupon/consumer/CustomKafkaConsumer.java
@@ -0,0 +1,14 @@
+package com.company.demo.giftcoupon.consumer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class CustomKafkaConsumer {
+    @KafkaListener(topics = "test-topic", groupId = "consumer-group1")
+    public void handleMessage(String message) {
+        System.out.println("Received message: " + message);
+    }
+}
