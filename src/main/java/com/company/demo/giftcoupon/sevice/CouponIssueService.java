d8f594fdcee22b031f358e5c50744b820b91cbdc
Shinjongyun <tigerjong123@gmail.com>
2025-07-18 13:47:58 +0900

    Feat: CouponIssueService 프로토타입

---
+++ src/main/java/com/company/demo/giftcoupon/sevice/CouponIssueService.java
@@ -0,0 +1,27 @@
+package com.company.demo.giftcoupon.sevice;
+
+import com.company.demo.giftcoupon.mapper.dto.request.CouponIssueRequest;
+import com.company.demo.giftcoupon.producer.CustomKafkaProducer;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class CouponIssueService {
+    private final CustomKafkaProducer customKafkaProducer;
+    private final CouponRespository couponRespository;
+
+    private static final String ISSUE_TOPIC = "coupon-issue";
+
+
+    @Transactional
+    public CouponIssueResponse issueCoupon(CouponIssueRequest request) {
+        customKafkaProducer.sendMessage(ISSUE_TOPIC, request);
+        Coupon coupon = Coupon.builder().build();
+        couponRespository.save(coupon);
+        return CouponIssueResponse.of(coupon);
+    }
+}
