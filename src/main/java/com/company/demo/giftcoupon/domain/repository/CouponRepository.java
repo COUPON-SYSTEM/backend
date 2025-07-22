0947fd2bc9426c14869cdbf93ff5f76586be65dc
Shinjongyun <tigerjong123@gmail.com>
2025-07-22 10:01:47 +0900

    Feat: CouponIssueResponse

--- src/main/java/com/company/demo/giftcoupon/domain/repository/CouponRepository.java
+++ src/main/java/com/company/demo/giftcoupon/domain/repository/CouponRepository.java
@@ -2,7 +2,9 @@ package com.company.demo.giftcoupon.domain.repository;
 
 import com.company.demo.giftcoupon.domain.entity.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
 
 }
