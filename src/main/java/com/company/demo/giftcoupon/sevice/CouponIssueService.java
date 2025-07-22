f3d1eb2393d61e1b2da83c4de0f4f02ee60d6223
Shinjongyun <tigerjong123@gmail.com>
2025-07-22 10:01:47 +0900

    Feat: CouponIssueResponse

--- src/main/java/com/company/demo/giftcoupon/sevice/CouponIssueService.java
+++ src/main/java/com/company/demo/giftcoupon/sevice/CouponIssueService.java
@@ -2,18 +2,20 @@ package com.company.demo.giftcoupon.sevice;
 
 import com.company.demo.giftcoupon.domain.entity.Coupon;
 import com.company.demo.giftcoupon.mapper.dto.request.CouponIssueRequest;
+import com.company.demo.giftcoupon.mapper.dto.response.CouponIssueResponse;
 import com.company.demo.giftcoupon.producer.CustomKafkaProducer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import com.company.demo.giftcoupon.domain.repository.CouponRepository;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class CouponIssueService {
     private final CustomKafkaProducer customKafkaProducer;
-    private final CouponRespository couponRespository;
+    private final CouponRepository couponRepository;
 
     @Transactional
     public CouponIssueResponse issueCoupon(CouponIssueRequest request) {
@@ -21,7 +23,7 @@ public class CouponIssueService {
         Coupon coupon = Coupon.codeOnlyBuilder()
                 .code("메롱")
                 .build();
-        couponRespository.save(coupon);
+        couponRepository.save(coupon);
         return CouponIssueResponse.of(coupon);
     }
 }
