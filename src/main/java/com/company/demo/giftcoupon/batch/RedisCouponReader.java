ec45aab70093415936d1387e5a2f117d70cbcac5
Shinjongyun <tigerjong123@gmail.com>
2025-07-25 17:02:59 +0900

    Refact: RedisCouponReader 1개 pop으로 변경

--- src/main/java/com/company/demo/giftcoupon/batch/RedisCouponReader.java
+++ src/main/java/com/company/demo/giftcoupon/batch/RedisCouponReader.java
@@ -1,13 +1,11 @@
 package com.company.demo.giftcoupon.batch;
 
+import com.company.demo.common.constant.RedisKey;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.stereotype.Component;
 
-import java.util.Collections;
-import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -15,20 +13,8 @@ public class RedisCouponReader implements ItemReader<String> {
 
     private final RedisTemplate<String, String> redisTemplate;
 
-    private static final String LUA_POP_10 = """
-        local items = redis.call('LRANGE', KEYS[1], 0, 9)
-        if #items > 0 then
-            redis.call('LTRIM', KEYS[1], 10, -1)
-        end
-        return items
-    """;
-
     @Override
     public String read() {
-        List<String> users = redisTemplate.execute(
-                new DefaultRedisScript<>(LUA_POP_10, List.class),
-                Collections.singletonList("coupon:queue")
-        );
-        return (users == null || users.isEmpty()) ? null : users;
+        return redisTemplate.opsForList().rightPop(RedisKey.COUPON_REQUEST_QUEUE_KEY);
     }
 }
