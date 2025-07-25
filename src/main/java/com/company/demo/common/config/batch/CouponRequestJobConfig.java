ab05d2f1573f8d0b9f3d0deb374c15e895e343dd
Shinjongyun <tigerjong123@gmail.com>
2025-07-25 16:41:16 +0900

    Feat: RedisCouponReader 10개씩 pop

--- src/main/java/com/company/demo/common/config/batch/CouponRequestJobConfig.java
+++ src/main/java/com/company/demo/common/config/batch/CouponRequestJobConfig.java
@@ -5,6 +5,7 @@ import org.springframework.batch.core.Step;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.batch.core.Job;
+import com.company.demo.giftcoupon.batch.RedisCouponReader;
 
 @Configuration
 @RequiredArgsConstructor
