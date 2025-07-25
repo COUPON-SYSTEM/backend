27fe5f241df6dcae63b5d37f0bce489919368ba9
Shinjongyun <tigerjong123@gmail.com>
2025-07-25 15:13:33 +0900

    Feat: RequestJobConfig

---
+++ src/main/java/com/company/demo/common/config/batch/CouponRequestJobConfig.java
@@ -0,0 +1,31 @@
+package com.company.demo.common.config.batch;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Step;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.batch.core.Job;
+
+@Configuration
+@RequiredArgsConstructor
+public class CouponRequestJobConfig {
+    private final RedisCouponReader redisCouponReader;
+    private final KafkaCouponPublisher kafkaCouponPublisher;
+    private final CouponWriter couponWriter;
+
+    @Bean
+    public Job couponRequestJob(JobBuilderFactory jobBuilderFactory,
+                              StepBuilderFactory stepBuilderFactory) {
+
+        Step step = stepBuilderFactory.get("couponIssueStep")
+                .<String, CouponRequest>chunk(10)
+                .reader(redisCouponReader)
+                .processor(kafkaCouponPublisher)
+                .writer(couponWriter)
+                .build();
+
+        return jobBuilderFactory.get("couponIssueJob")
+                .start(step)
+                .build();
+    }
+}
