faec7db44d2e4b6967045c85493fcf98fc78fb69
Shinjongyun <tigerjong123@gmail.com>
2025-07-25 17:05:19 +0900

    Feat: KafkaCouponWriter에 누적 쿠폰수 증가 로직

--- src/main/java/com/company/demo/giftcoupon/batch/KafkaCouponWriter.java
+++ src/main/java/com/company/demo/giftcoupon/batch/KafkaCouponWriter.java
@@ -1,11 +1,13 @@
 package com.company.demo.giftcoupon.batch;
 
 import com.company.demo.common.constant.KafkaTopic;
+import com.company.demo.common.constant.RedisKey;
 import com.company.demo.giftcoupon.event.CouponRequestEvent;
 import com.company.demo.giftcoupon.producer.CustomKafkaProducer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.item.Chunk;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -15,11 +17,16 @@ import java.util.List;
 public class KafkaCouponWriter implements ItemWriter<CouponRequestEvent> {
 
     private final CustomKafkaProducer kafkaProducer;
+    private final RedisTemplate<String, String> redisTemplate;
 
     @Override
     public void write(Chunk<? extends CouponRequestEvent> items) {
         for (CouponRequestEvent event : items) {
             kafkaProducer.sendRequestMessage(event);
         }
+
+        // Kafka 메시지 전송이 끝난 뒤 누적 발급 수 증가
+        // 즉, 쿠폰 발행했다라는 것을 Kafka 메시지 발행을 기준으로 함
+        redisTemplate.opsForValue().increment(RedisKey.COUPON_REQUEST_COUNT_KEY, items.size());
     }
 }
