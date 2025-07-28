ee88acb35650c6a410467378505a7e517cf16070
Shinjongyun <tigerjong123@gmail.com>
2025-07-28 17:58:33 +0900

    Refact: Avro 방식에서 json 방식으로 변경

--- src/main/java/com/company/demo/common/config/kafka/KafkaConsumerConfig.java
+++ src/main/java/com/company/demo/common/config/kafka/KafkaConsumerConfig.java
@@ -9,12 +9,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
 import com.company.demo.giftcoupon.event.CouponRequestEvent;
-import io.confluent.kafka.serializers.KafkaAvroSerializer;
-import io.lettucconfluent.kafka.serializers.KafkaAvroDeserializer;
+import io.confluent.kafka.serializers.KafkaAvroDeserializer;
 import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig;
+import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig;
 import org.apache.avro.specific.SpecificRecord;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -28,46 +28,21 @@ public class KafkaConsumerConfig {
     @Value("${spring.kafka.consumer.group-id}")
     private String groupId;
 
-    //private final KafkaErrorHandler kafkaErrorHandler;
+    @Value("${spring.kafka.properties.schema.registry.url}")
+    private String schemaRegistryUrl;
 
-//    public <T> ConsumerFactory<String, T> consumerFactory(Class<T> clazz) {
-//        JsonDeserializer<T> deserializer = new JsonDeserializer<>(clazz);
-//        deserializer.addTrustedPackages("*");
-//
-//        Map<String, Object> props = new HashMap<>();
-//        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-//        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-//        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
-//        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-//        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-//
-//        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), deserializer);
-//    }
-//
-//    @Bean
-//    public ConcurrentKafkaListenerContainerFactory<String, CouponRequestEvent> couponRequestEventListenerFactory() {
-//        ConcurrentKafkaListenerContainerFactory<String, CouponRequestEvent> factory =
-//                new ConcurrentKafkaListenerContainerFactory<>();
-//        factory.setConsumerFactory(consumerFactory(CouponRequestEvent.class));
-//        return factory;
-//    }
+    // private final KafkaErrorHandler kafkaErrorHandler;
 
-    public <T extends SpecificRecord> ConsumerFactory<String, T> avroConsumerFactory(Class<T> clazz) {
+    public <T> ConsumerFactory<String, T> consumerFactory(Class<T> clazz) {
+        JsonDeserializer<T> deserializer = new JsonDeserializer<>(clazz);
+        deserializer.addTrustedPackages("*");
+
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-
-        // Schema Registry 주소
-        props.put(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://localhost:8081");
-
-        // Avro 클래스로 deserialize 하려면 필수!
-        props.put(KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG, true);
-
-        KafkaAvroDeserializer deserializer = new KafkaAvroDeserializer();
-        deserializer.configure(props, false); // false → value deserializer
 
         return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), deserializer);
     }
@@ -76,7 +51,32 @@ public class KafkaConsumerConfig {
     public ConcurrentKafkaListenerContainerFactory<String, CouponRequestEvent> couponRequestEventListenerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, CouponRequestEvent> factory =
                 new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(avroConsumerFactory(CouponRequestEvent.class));
+        factory.setConsumerFactory(consumerFactory(CouponRequestEvent.class));
         return factory;
     }
+
+//    public <T extends SpecificRecord> ConsumerFactory<String, T> avroConsumerFactory(Class<T> clazz) {
+//        Map<String, Object> props = new HashMap<>();
+//        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+//        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+//        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class);
+//        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+//        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+//
+//        // Schema Registry 주소
+//        props.put(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryUrl);
+////
+//        // Avro 클래스로 deserialize 하려면 필수!
+//        props.put(KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG, true);
+//
+//        return new DefaultKafkaConsumerFactory<>(props);
+//    }
+//
+//    @Bean
+//    public ConcurrentKafkaListenerContainerFactory<String, CouponRequestEvent> couponRequestEventListenerFactory() {
+//        ConcurrentKafkaListenerContainerFactory<String, CouponRequestEvent> factory =
+//                new ConcurrentKafkaListenerContainerFactory<>();
+//        factory.setConsumerFactory(this.<CouponRequestEvent>avroConsumerFactory());
+//        return factory;
+//    }
 }
