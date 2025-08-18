8a7e37f3d65e55c61680d8fbb33b2bd4be3840a0
gongPyeon <pyeonkang@naver.com>
2025-08-18 09:26:33 +0900

    feat: 응답 형식 설정

--- src/main/java/com/company/demo/common/response/ApiError.java
+++ src/main/java/com/company/demo/common/response/ApiError.java
@@ -1,4 +1,10 @@
 package com.company.demo.common.response;
 
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class ApiError {
+    private String code;
+    private String message;
 }
--- src/main/java/com/company/demo/common/response/ApiResponse.java
+++ src/main/java/com/company/demo/common/response/ApiResponse.java
@@ -1,4 +1,40 @@
 package com.company.demo.common.response;
 
-public class ApiResponse {
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
+
+@ToString(of = "success, data, error")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ApiResponse<T> {
+    private boolean success;
+    private T data;
+    private ApiError error;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    private LocalDateTime serverDateTime = LocalDateTime.now();
+
+    private ApiResponse(boolean success, T data, ApiError error) {
+        this.success = success;
+        this.data = data;
+        this.error = error;
+    }
+
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>(true, data, null);
+    }
+
+    public static ApiResponse<Object> success() {
+        return new ApiResponse<>(true, null, null);
+    }
+
+    public static ApiResponse<Object> error(String code, String message) {
+        return new ApiResponse<>(false, null, new ApiError(code, message));
+    }
+
+    public static ApiResponse<Object> error(ApiError error) {
+        return new ApiResponse<>(false, null, error);
+    }
+
 }
+
