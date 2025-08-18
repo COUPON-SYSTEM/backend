a548a073768c08fa19de487689e6dac3f4ddf645
Shinjongyun <tigerjong123@gmail.com>
2025-08-18 17:08:19 +0900

    feat: TryIssueCouponCommand

--- src/main/java/com/company/demo/giftcoupon/sevice/CouponIssueService.java
+++ src/main/java/com/company/demo/giftcoupon/sevice/CouponIssueService.java
@@ -2,6 +2,7 @@ package com.company.demo.giftcoupon.sevice;
 
 import com.company.demo.giftcoupon.domain.entity.Coupon;
 import com.company.demo.giftcoupon.mapper.dto.request.CouponIssuanceRequestDto;
+import com.company.demo.giftcoupon.mapper.dto.request.TryIssueCouponCommand;
 import com.company.demo.giftcoupon.mapper.dto.response.CouponIssueResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,12 +20,12 @@ public class CouponIssueService {
     private final ApplicationEventPublisher applicationEventPublisher;
 
     @Transactional
-    public void tryToIssueCoupon(final CouponIssuanceRequestDto requestDto) {
+    public void tryToIssueCoupon(final TryIssueCouponCommand comman) {
 
         // 1. 선착순 쿠폰 발급 시도(발급 여부 검증)
         // CouponIssuanceResult result = timeAttackCouponIssuer.tryIssuance(requestDto);
         CouponIssueResponseDto result = this.issueCoupon(requestDto);
-        ApplicationEvent
+
         // 2. Spring Event 발행
         if (result.isSuccess()) {
             DomainEventEnvelop envelop = result.toEvent();
@@ -32,7 +33,7 @@ public class CouponIssueService {
         }
     }
 
-    private CouponIssueResponseDto issueCoupon(CouponIssuanceRequestDto request) {
+    private CouponIssueResponseDto issueCoupon(TryIssueCouponCommand request) {
         Coupon coupon = Coupon.codeOnlyBuilder()
                 .code("메롱")
                 .build();
