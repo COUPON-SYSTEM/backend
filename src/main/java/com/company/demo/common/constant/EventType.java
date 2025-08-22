f3ca258dd74505be4ae5d1c9493f71ec2da6bd5d
Shinjongyun <tigerjong123@gmail.com>
2025-08-22 16:43:15 +0900

    feat: record 방식으로 command, event 구현

---
+++ src/main/java/com/company/demo/common/constant/EventType.java
@@ -0,0 +1,8 @@
+package com.company.demo.common.constant;
+
+public final class EventType {
+
+    public static final String ISSUED_EVENT = "CouponIssuedEvent";
+
+    private EventType() {}
+}
--- src/main/java/com/company/demo/giftcoupon/outbox/domain/event/CouponIssuedPayload.java
+++ src/main/java/com/company/demo/giftcoupon/outbox/domain/event/CouponIssuedPayload.java
@@ -1,9 +1,20 @@
 package com.company.demo.giftcoupon.outbox.domain.event;
 
+import lombok.Builder;
+
 import java.time.LocalDateTime;
 
 public record CouponIssuedPayload(
         Long memberId,
         Long couponId,
         LocalDateTime issuedAt
-) {}
+) {
+    @Builder
+    public static CouponIssuedPayload of(Long memberId, Long couponId, LocalDateTime issuedAt) {
+        return CouponIssuedPayload.builder()
+                .memberId(memberId)
+                .couponId(couponId)
+                .issuedAt(issuedAt)
+                .build();
+    }
+}
