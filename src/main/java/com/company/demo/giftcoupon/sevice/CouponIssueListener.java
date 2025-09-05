ced5218590ecadf6970463abfd7ca25769d85dd6
gongPyeon <pyeonkang@naver.com>
2025-09-05 11:14:16 +0900

    feat: NotificationSystem 전체 구조 설정

--- src/main/java/com/company/demo/giftcoupon/sevice/CouponIssueListener.java
+++ src/main/java/com/company/demo/giftcoupon/sevice/CouponIssueListener.java
@@ -1,6 +1,7 @@
 package com.company.demo.giftcoupon.sevice;
 
 import com.company.demo.giftcoupon.domain.entity.Coupon;
+import com.company.demo.giftcoupon.event.CouponIssuedEvent;
 import com.company.demo.giftcoupon.mapper.dto.request.CouponIssueRequest;
 import com.company.demo.giftcoupon.mapper.dto.response.CouponIssueResponse;
 import com.company.demo.giftcoupon.producer.CustomKafkaProducer;
@@ -14,9 +15,11 @@ import com.company.demo.giftcoupon.domain.repository.CouponRepository;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class CouponIssueService {
+public class CouponIssueListener {
     private final CustomKafkaProducer customKafkaProducer;
     private final CouponRepository couponRepository;
+    private final HistoryListener historyListener;
+    private final NotificationListener notificationListener;
 
     @Transactional
     public CouponIssueResponse issueCoupon(CouponIssueRequest request) {
@@ -30,5 +33,11 @@ public class CouponIssueService {
         // 2. Kafka에 "발급 완료" 메시지 발행
         //CouponIssuedEvent event = new CouponIssuedEvent(userId, ...);
         // kafkaTemplate.send("coupon-issued-topic", event);
+    }
+
+    // TODO: Transactional 단위를 어떻게 할지
+    public void notificationCoupon(CouponIssuedEvent event) {
+        historyListener.issuedHistorySave(event);
+        notificationListener.notification(event);
     }
 }
