ff047dbc8fd18aba743a208b4ca9356e3b09492e
gongPyeon <pyeonkang@naver.com>
2025-09-10 16:02:58 +0900

    feat: sse 핸들러 구현

--- src/main/java/com/company/demo/giftcoupon/event/FinalizableEvent.java
+++ src/main/java/com/company/demo/giftcoupon/event/FinalizableEvent.java
@@ -2,7 +2,7 @@ package com.company.demo.giftcoupon.event;
 
 import java.time.LocalDateTime;
 
-public interface FinalizableEvent { // TODO: 이벤트의 공통처리를 위한 인터페이스 (이게 최선일까?)
+public interface FinalizableEvent { // TODO: 이벤트의 공통처리를 위한 인터페이스 - zero payload의 필요성
     Long getCouponId();
     Long getUserId();
     String getCouponCode();
