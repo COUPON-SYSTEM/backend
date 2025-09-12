f60a6f6650a1c285c2fc4b9cbf90141c8cf5f3b2
Shinjongyun <tigerjong123@gmail.com>
2025-09-12 15:18:45 +0900

    feat: Kafka 메시지를 전솔하는 리스너 구현

--- src/main/java/com/company/demo/giftcoupon/outbox/domain/event/CouponIssuedPayload.java
+++ src/main/java/com/company/demo/giftcoupon/outbox/domain/event/CouponIssuedPayload.java
@@ -1,6 +1,7 @@
 package com.company.demo.giftcoupon.outbox.domain.event;
 
 import lombok.Builder;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
@@ -15,6 +16,19 @@ public record CouponIssuedPayload(
                 .memberId(memberId)
                 .couponId(couponId)
                 .issuedAt(issuedAt)
+                .build();
+    }
+    @Builder
+    public static CouponIssuedPayload from(DomainEventEnvelope<?> env) {
+        // 안전하게 캐스팅
+        if (!(env.payload() instanceof CouponIssuedPayload payload)) {
+            throw new IllegalArgumentException("Invalid payload type: " + env.payload().getClass());
+        }
+
+        return CouponIssuedPayload.builder()
+                .memberId(payload.memberId())
+                .couponId(payload.couponId())
+                .issuedAt(payload.issuedAt())
                 .build();
     }
 }
---
+++ src/main/java/com/company/demo/giftcoupon/outbox/listener/CouponIssuedEventMessageListener.java
@@ -0,0 +1,23 @@
+package com.company.demo.giftcoupon.outbox.listener;
+
+import com.company.demo.giftcoupon.outbox.domain.event.CouponIssuedPayload;
+import com.company.demo.giftcoupon.outbox.domain.event.DomainEventEnvelope;
+import com.company.demo.giftcoupon.producer.CustomKafkaProducer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+@Component
+@RequiredArgsConstructor
+public class CouponIssuedEventMessageListener {
+    private final CustomKafkaProducer customKafkaProducer;
+
+    // 2. 카프카에 메시지 전송 (TransactionPhase.AFTER_COMMIT)
+    @Async(EVENT_ASYNC_TASK_EXECUTOR)
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void sendMessageHandler(DomainEventEnvelope<?> env) {
+        customKafkaProducer.sendIssuedMessage(CouponIssuedPayload.from(env));
+    }
+}
