67cb3027a8bdf58a74ed591dee9a59b9beb5857a
Shinjongyun <tigerjong123@gmail.com>
2025-10-02 16:36:52 +0900

    refact: CouponRequestProcessor

--- src/main/java/com/company/demo/giftcoupon/batch/CouponRequestProcessor.java
+++ src/main/java/com/company/demo/giftcoupon/batch/CouponRequestProcessor.java
@@ -1,26 +1,37 @@
 package com.company.demo.giftcoupon.batch;
 
 import com.company.demo.common.constant.EventType;
+import com.company.demo.common.constant.Source;
+import com.company.demo.giftcoupon.domain.entity.Coupon;
 import com.company.demo.giftcoupon.event.CouponIssueEvent;
 import com.company.demo.giftcoupon.outbox.domain.event.CouponIssuedEvent;
+import com.company.demo.giftcoupon.outbox.domain.event.CouponIssuedPayload;
+import com.company.demo.giftcoupon.outbox.domain.event.DomainEventEnvelope;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.batch.item.ItemProcessor;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class CouponRequestProcessor implements ItemProcessor<String, CouponIssueEvent> {
+public class CouponRequestProcessor implements ItemProcessor<String, ProcessedCouponData> {
 
     @Override
-    public CouponIssueEvent process(String memberId) {
-        return new CouponIssueEvent(
-                null,
-                memberId,
-                EventType.ISSUE_EVENT,
-                LocalDateTime.now());
+    public ProcessedCouponData process(String memberId) {
+        // 1) 쿠폰 미리 구성 (DB 저장 금지)
+        Coupon coupon = Coupon.builder()
+                .memberId(Long.valueOf(memberId))
+                .code(Source.COUPON_ISSUED)
+                .build();
+
+        // 2) Writer에게 전달할 재료 반환
+        return ProcessedCouponData.builder()
+                .coupon(coupon)
+                .event(CouponIssuedEvent.of(memberId, EventType.ISSUED_EVENT))
+                .build();
     }
 }
--- src/main/java/com/company/demo/giftcoupon/batch/ProcessedCouponData.java
+++ src/main/java/com/company/demo/giftcoupon/batch/ProcessedCouponData.java
@@ -1,11 +1,14 @@
 package com.company.demo.giftcoupon.batch;
 
 import com.company.demo.giftcoupon.domain.entity.Coupon;
+import com.company.demo.giftcoupon.outbox.domain.event.CouponIssuedEvent;
+import com.company.demo.giftcoupon.outbox.domain.event.CouponIssuedPayload;
+import com.company.demo.giftcoupon.outbox.domain.event.DomainEventEnvelope;
 import lombok.Builder;
 
 
 @Builder
 public record ProcessedCouponData(
-        Coupon coupon,                                            // 아직 미저장 엔티티
-        DomainEventEnvelope<CouponIssuedPayload> envelope          // Outbox에 기록할 이벤트 봉투
+        Coupon coupon,                 // 아직 미저장 엔티티
+        CouponIssuedEvent event        // 이벤트
 ) { }
