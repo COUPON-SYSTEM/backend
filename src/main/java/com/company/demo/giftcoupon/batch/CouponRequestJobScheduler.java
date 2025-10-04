3e00a112fc0ebea1fc7263787c9ebcb059b663b5
Shinjongyun <tigerjong123@gmail.com>
2025-10-04 23:50:57 +0900

    feat: redis에 없으면 job 스킵

--- src/main/java/com/company/demo/giftcoupon/batch/CouponRequestJobScheduler.java
+++ src/main/java/com/company/demo/giftcoupon/batch/CouponRequestJobScheduler.java
@@ -1,13 +1,18 @@
 package com.company.demo.giftcoupon.batch;
 
+import com.company.demo.common.constant.RedisKey;
+import com.company.demo.giftcoupon.domain.repository.CouponRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Slf4j
 @Component
@@ -16,9 +21,17 @@ public class CouponRequestJobScheduler {
 
     private final JobLauncher jobLauncher;
     private final Job couponRequestJob;
+    private final RedisTemplate<String, String> redisTemplate;
 
     @Scheduled(fixedRate = 10000) // 10초마다 실행
     public void runJob() {
+        long len = Optional.ofNullable(
+                redisTemplate.opsForList().size(RedisKey.COUPON_REQUEST_QUEUE_KEY)
+        ).orElse(0L);
+        if (len == 0L) {
+            log.debug("[CouponScheduler] queue empty. skip running job.");
+            return;
+        }
         try {
             JobParameters jobParameters = new JobParametersBuilder()
                     .addLong("timestamp", System.currentTimeMillis()) // 매번 다른 JobParameters 필요
