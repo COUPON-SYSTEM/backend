19b5883fcfca6cbc110e4bb1b2b1353c0cd14e3f
Shinjongyun <tigerjong123@gmail.com>
2026-02-19 16:18:04 +0900

    feat: 나의 쿠폰 조회 api 추가

--- src/main/java/com/company/demo/giftcoupon/domain/repository/CouponRepository.java
+++ src/main/java/com/company/demo/giftcoupon/domain/repository/CouponRepository.java
@@ -4,7 +4,11 @@ import com.company.demo.giftcoupon.domain.entity.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
     long countIssuedByEventType(String eventType);
+
+    List<Coupon> findByUserId(Long userId);
 }
